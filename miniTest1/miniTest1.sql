de4d75aa5b575d2a20790e41485617c42fd56122
mtuancsp <minhtuan267@gmail.com>
2023-05-08 11:05:50 +0700

    minitest1

--- miniTest1/miniTest1.sql
+++ miniTest1/miniTest1.sql
@@ -1,100 +1,104 @@
-
 -- Tạo db ---------------------------------------------------------------
 create database QuanLyHocVien;
 use QuanLyHocVien;
 
-CREATE TABLE Address (
-  id INT PRIMARY KEY,
-  address VARCHAR(45)
+CREATE TABLE Address
+(
+    id      INT PRIMARY KEY,
+    address VARCHAR(45)
 );
 
-CREATE TABLE Classes (
-  id INT PRIMARY KEY,
-  name VARCHAR(45),
-  language VARCHAR(45),
-  description VARCHAR(45)
+CREATE TABLE Classes
+(
+    id          INT PRIMARY KEY,
+    name        VARCHAR(45),
+    language    VARCHAR(45),
+    description VARCHAR(45)
 );
 
-CREATE TABLE Students (
-  id INT PRIMARY KEY,
-  fullname VARCHAR(45),
-  address_id INT,
-  age INT,
-  phone CHAR(10) UNIQUE,
-  classes_id INT,
-  FOREIGN KEY (address_id) REFERENCES Address(id),
-  FOREIGN KEY (classes_id) REFERENCES Classes(id)
+CREATE TABLE Students
+(
+    id         INT PRIMARY KEY,
+    fullname   VARCHAR(45),
+    address_id INT,
+    age        INT,
+    phone      CHAR(10) UNIQUE,
+    classes_id INT,
+    FOREIGN KEY (address_id) REFERENCES Address (id),
+    FOREIGN KEY (classes_id) REFERENCES Classes (id)
 );
 
-CREATE TABLE Course (
-  id INT PRIMARY KEY,
-  name VARCHAR(45),
-  description VARCHAR(45)
+CREATE TABLE Course
+(
+    id          INT PRIMARY KEY,
+    name        VARCHAR(45),
+    description VARCHAR(45)
 );
 
-CREATE TABLE Point (
-  id INT PRIMARY KEY,
-  course_id INT,
-  student_id INT,
-  point INT,
-  FOREIGN KEY (course_id) REFERENCES Course(id),
-  FOREIGN KEY (student_id) REFERENCES Students(id)
+CREATE TABLE Point
+(
+    id         INT PRIMARY KEY,
+    course_id  INT,
+    student_id INT,
+    point      INT,
+    FOREIGN KEY (course_id) REFERENCES Course (id),
+    FOREIGN KEY (student_id) REFERENCES Students (id)
 );
 
 -- Thêm 5 bản ghi vào bảng Address
-INSERT INTO Address (id, address) VALUES
-(1, '123 Đường A, Quận 1, HN'),
-(2, '456 Đường B, Quận 2, TB'),
-(3, '789 Đường C, Quận 3, TP. HCM'),
-(4, '1011 Đường D, Quận 4, HP'),
-(5, '1213 Đường E, Quận 5, ĐN');
+INSERT INTO Address (id, address)
+VALUES (1, '123 Đường A, Quận 1, HN'),
+       (2, '456 Đường B, Quận 2, TB'),
+       (3, '789 Đường C, Quận 3, TP. HCM'),
+       (4, '1011 Đường D, Quận 4, HP'),
+       (5, '1213 Đường E, Quận 5, ĐN');
 
 -- Thêm 5 bản ghi vào bảng Course
-INSERT INTO Course (id, name, description) VALUES
-(1, 'Toán học', 'Giới thiệu về toán học'),
-(2, 'Vật lý', 'Các nguyên lý cơ bản của vật lý'),
-(3, 'Hóa học', 'Giới thiệu về hóa học'),
-(4, 'Lịch sử', 'Những sự kiện lịch sử quan trọng'),
-(5, 'Ngoại ngữ', 'Học ngoại ngữ cơ bản');
+INSERT INTO Course (id, name, description)
+VALUES (1, 'Toán học', 'Giới thiệu về toán học'),
+       (2, 'Vật lý', 'Các nguyên lý cơ bản của vật lý'),
+       (3, 'Hóa học', 'Giới thiệu về hóa học'),
+       (4, 'Lịch sử', 'Những sự kiện lịch sử quan trọng'),
+       (5, 'Ngoại ngữ', 'Học ngoại ngữ cơ bản');
 
 -- Thêm 5 bản ghi vào bảng Classes
-INSERT INTO Classes (id, name, language, description) VALUES
-(1, 'Toán', 'Tiếng Việt', 'Học toán cơ bản'),
-(2, 'Vật lý', 'Tiếng Anh', 'Học vật lý cơ bản'),
-(3, 'Hóa học', 'Tiếng Việt', 'Học hóa học cơ bản'),
-(4, 'Lịch sử', 'Tiếng Việt', 'Học lịch sử thế giới'),
-(5, 'Ngoại ngữ', 'Tiếng Anh', 'Học tiếng Anh cơ bản');
+INSERT INTO Classes (id, name, language, description)
+VALUES (1, 'Toán', 'Tiếng Việt', 'Học toán cơ bản'),
+       (2, 'Vật lý', 'Tiếng Anh', 'Học vật lý cơ bản'),
+       (3, 'Hóa học', 'Tiếng Việt', 'Học hóa học cơ bản'),
+       (4, 'Lịch sử', 'Tiếng Việt', 'Học lịch sử thế giới'),
+       (5, 'Ngoại ngữ', 'Tiếng Anh', 'Học tiếng Anh cơ bản');
 
 -- Thêm 10 bản ghi vào bảng Students
-INSERT INTO Students (id, fullname, address_id, age, phone, classes_id) VALUES
-(1, 'Nguyễn Văn A', 1, 18, '0123456789', 1),
-(2, 'Trần Thị B', 2, 19, '0234567891', 2),
-(3, 'Lê Văn C', 3, 20, '0345678912', 3),
-(4, 'Phạm Thị D', 4, 21, '0456789123', 4),
-(5, 'Nguyễn Thanh E', 5, 22, '0567891234', 5),
-(6, 'Hoàng Văn F', 1, 18, '0678912345', 1),
-(7, 'Lê Thị G', 2, 19, '0789123456', 2),
-(8, 'Trần Văn H', 3, 20, '0891234567', 3),
-(9, 'Nguyễn Thị I', 5, 22, '0912345678', 4),
-(10, 'Trần Thanh J', 1, 18, '0111111111', 5);
+INSERT INTO Students (id, fullname, address_id, age, phone, classes_id)
+VALUES (1, 'Nguyễn Văn A', 1, 18, '0123456789', 1),
+       (2, 'Trần Thị B', 2, 19, '0234567891', 2),
+       (3, 'Lê Văn C', 3, 20, '0345678912', 3),
+       (4, 'Phạm Thị D', 4, 21, '0456789123', 4),
+       (5, 'Nguyễn Thanh E', 5, 22, '0567891234', 5),
+       (6, 'Hoàng Văn F', 1, 18, '0678912345', 1),
+       (7, 'Lê Thị G', 2, 19, '0789123456', 2),
+       (8, 'Trần Văn H', 3, 20, '0891234567', 3),
+       (9, 'Nguyễn Thị I', 5, 22, '0912345678', 4),
+       (10, 'Trần Thanh J', 1, 18, '0111111111', 5);
 
 -- Thêm 15 bản ghi vào bảng Point
-INSERT INTO Point (id, course_id, student_id, point) VALUES
-(1, 1, 1, 8),
-(2, 1, 2, 9),
-(3, 1, 3, 7),
-(4, 1, 4, 8),
-(5, 1, 5, 9),
-(6, 2, 6, 7),
-(7, 2, 7, 8),
-(8, 2, 8, 9),
-(9, 2, 9, 8),
-(10, 2, 10, 7),
-(11, 3, 1, 9),
-(12, 3, 2, 8),
-(13, 3, 3, 7),
-(14, 3, 4, 9),
-(15, 3, 5, 8);
+INSERT INTO Point (id, course_id, student_id, point)
+VALUES (1, 1, 1, 8),
+       (2, 1, 2, 9),
+       (3, 1, 3, 7),
+       (4, 1, 4, 8),
+       (5, 1, 5, 9),
+       (6, 2, 6, 7),
+       (7, 2, 7, 8),
+       (8, 2, 8, 9),
+       (9, 2, 9, 8),
+       (10, 2, 10, 7),
+       (11, 3, 1, 9),
+       (12, 3, 2, 8),
+       (13, 3, 3, 7),
+       (14, 3, 4, 9),
+       (15, 3, 5, 8);
 
 -- QUERY ----------------------------------------------------------------
 
@@ -132,6 +136,26 @@ from students s
          join address a on s.address_id = a.id
 group by a.address;
 
+-- (Những) Khóa học có điểm trung bình cao nhất
+SELECT course.name, avg(point.point) AS highest_avg_point
+FROM point
+         join course on point.course_id = course.id
+GROUP BY point.course_id
+HAVING avg(point.point) = (SELECT avg(point.point) AS avg_point
+                           FROM point
+                           GROUP BY point.course_id
+                           ORDER BY avg_point DESC
+                           LIMIT 1);
+
+SELECT course.name, round(avg(point.point), 3) AS highest_avg_point
+FROM point
+         JOIN course ON point.course_id = course.id
+GROUP BY point.course_id
+HAVING avg(point.point) = (SELECT MAX(avg_point)
+                           FROM (SELECT avg(point.point) AS avg_point
+                                 FROM point
+                                 GROUP BY point.course_id) AS avg_points);
+
 -- Tạo view tính điểm trung bình của các khóa học
 create or replace view avgPoints as
 select c.name as Course, ROUND(AVG(point), 3) as avg_point
@@ -143,19 +167,25 @@ set @max_avg = (SELECT MAX(avg_point)
                 FROM avgPoints);
 
 -- Điểm trung bình của các khóa học
-select * from avgPoints;
+select *
+from avgPoints;
 
 -- Khóa học có điểm trung bình cao nhất
-select * from avgPoints
+select *
+from avgPoints
 where avg_point = @max_avg;
 
-
-
-
-
-
-
-
-
-
+-- Đưa ra khóa học có điểm trung bình cao nhất
+SELECT c.name, ROUND(p.avg_point, 2) AS 'Điểm_trung_bình'
+FROM (SELECT course_id, AVG(point) AS avg_point
+      FROM point
+      GROUP BY course_id)
+         AS p
+         JOIN course AS c
+              ON p.course_id = c.id
+         JOIN (SELECT MAX(avg_point) AS max_point
+               FROM (SELECT AVG(point) AS avg_point
+                     FROM point
+                     GROUP BY course_id) AS t)
+    AS m ON p.avg_point = m.max_point;
 
--- src/p5/onTap/tongHop.sql
+++ src/p5/onTap/tongHop.sql
@@ -115,25 +115,25 @@ SELECT CONCAT_WS(
                addressLine2,
                CONCAT_WS(' ', postalCode, city),
                country
-               ) AS Customer_Address
+           ) AS Customer_Address
 FROM customers;
 
 -- REPLACE sửa lỗi chính tả, thay thế chuỗi trong bảng Product
 UPDATE products
-SET productDescription = REPLACE(productDescription,'abuot','about');
+SET productDescription = REPLACE(productDescription, 'abuot', 'about');
 
 -- sử dụng IF để hiển thị trạng thái của khách hàng là N / A nếu nó là NULL
 SELECT customerNumber,
        customerName,
-       IF(state IS NULL,'N/A',state) as state,
+       IF(state IS NULL, 'N/A', state) as state,
        -- hoặc dùng coalesce nếu tình huống ngắn gọn
        -- COALESCE(state, 'N/A') AS state,
        country
 FROM customers;
 
 -- IF cũng rất hữu ích với chức năng tổng hợp
-SELECT SUM(IF(status = 'Shipped',1,0)) AS Shipped,
-       SUM(IF(status = 'Cancelled',1,0)) AS Cancelled
+SELECT SUM(IF(status = 'Shipped', 1, 0))   AS Shipped,
+       SUM(IF(status = 'Cancelled', 1, 0)) AS Cancelled
 FROM orders;
 
 -- hoặc đơn giản hơn
@@ -144,7 +144,7 @@ FROM orders;
 -- tính toán số ngày giữa ngày vận chuyển và ngày yêu cầu trong đơn đặt hàng
 -- chúng ta sử dụng DATEDIFF
 SELECT orderNumber,
-       DATEDIFF(requiredDate,shippedDate) AS daysLeft
+       DATEDIFF(requiredDate, shippedDate) AS daysLeft
 FROM orders
 ORDER BY daysLeft;
 
@@ -155,14 +155,17 @@ ORDER BY daysLeft;
 
 -- các đơn đặt hàng trong khoảng 30 ngày tính từ ngày 1/4/2005
 SET @startDate = '2005-05-01';
-SELECT * FROM orders
+SELECT *
+FROM orders
 WHERE orderDate BETWEEN @startDate AND ADDDATE(@startDate, INTERVAL 30 DAY);
 SET @startDate2 = '2005-04-01';
-SELECT * FROM orders
+SELECT *
+FROM orders
 WHERE orderDate BETWEEN DATE_ADD(@startDate2, INTERVAL -1 MONTH) AND @startDate2;
 
 -- các đơn hàng năm 2005
-SELECT * FROM orders
+SELECT *
+FROM orders
 WHERE YEAR(orderDate) = 2005;
 # WHERE orderDate >= '2004-01-01' AND orderDate < '2005-01-01'
 -- câu WHERE này sẽ cho hiệu năng tốt hơn
@@ -170,7 +173,8 @@ WHERE YEAR(orderDate) = 2005;
 -- các đơn hàng theo từng quý năm 2004
 SELECT QUARTER(orderDate) AS Quý, COUNT(*) AS 'Số đơn hàng'
 FROM orders
-WHERE orderDate >= '2004-01-01' AND orderDate < '2005-01-01'
+WHERE orderDate >= '2004-01-01'
+  AND orderDate < '2005-01-01'
 GROUP BY Quý;
 
 -- Lấy ra 50 ký tự đầu tiên của phần mô tả sản phẩm, đặt tên là ‘Title of products’
@@ -180,35 +184,253 @@ SELECT productName,
 FROM products;
 
 -- Đưa ra mô tả về các nhân viên theo định dạng ‘Fullname, jobTitle.’
-SELECT CONCAT(Lastname, ', ',Firstname) AS Fullname, jobtitle
+SELECT CONCAT(Lastname, ', ', Firstname) AS Fullname, jobtitle
 FROM employees;
 
 -- Thay thế toàn bộ tên nhóm hàng có chứa ‘Cars’ thành ‘Automobiles’
 -- TH1 nếu không có ràng buộc ON UPDATE CASCADE
 INSERT INTO productlines
-SELECT REPLACE(productLine,'Automobiles', 'Cars' ),textDescription, htmlDescription, image
+SELECT REPLACE(productLine, 'Automobiles', 'Cars'), textDescription, htmlDescription, image
 FROM productlines
 WHERE productLine LIKE '%Automobiles%';
 
 UPDATE products
-SET productLine = REPLACE(productLine,'Automobiles', 'Cars')
+SET productLine = REPLACE(productLine, 'Automobiles', 'Cars')
 WHERE productLine IN (SELECT productLine FROM productlines WHERE productLine LIKE '%Automobiles%');
 
-DELETE FROM productlines WHERE productLine LIKE '%Automobiles%';
+DELETE
+FROM productlines
+WHERE productLine LIKE '%Automobiles%';
 
 -- TH2 nếu có ràng buộc ON UPDATE CASCADE
 UPDATE productlines
-SET productLine = REPLACE(productLine,'Cars', 'Automobiles')
+SET productLine = REPLACE(productLine, 'Cars', 'Automobiles')
 WHERE productLine LIKE '%Cars%';
 
 
 -- Tìm 5 đơn hàng được vận chuyển sớm nhất so với ngày yêu cầu
-SELECT orderNumber, DATEDIFF(requiredDate,shippedDate) AS daysLeft
+SELECT orderNumber, DATEDIFF(requiredDate, shippedDate) AS daysLeft
 FROM orders
 ORDER BY daysLeft DESC
 LIMIT 5;
 
 -- các đơn đặt hàng trong tháng 5 năm 2005 và có ngày chuyển hàng đến chưa xác định
-SELECT * FROM orders
-WHERE orderDate BETWEEN '2005-05-01' AND '2005-05-31' AND shippedDate IS NULL;
+SELECT *
+FROM orders
+WHERE orderDate BETWEEN '2005-05-01' AND '2005-05-31'
+  AND shippedDate IS NULL;
+
+-- tổng số tiền đã thu được
+SELECT sum(priceEach * quantityOrdered) total
+FROM orderdetails;
+
+--  mức giá cao nhất và mức giá nhỏ nhất của sản phẩm
+SELECT MAX(buyPrice) as highest_price,
+       MIN(buyPrice) as lowest_price
+FROM Products;
+
+-- số đơn đặt hàng trong từng trạng thái
+SELECT status, count(*)
+FROM orders
+GROUP BY status;
+
+-- Số loại sản phẩm trong mỗi loại dòng sản phẩm
+SELECT productLine, count(*)
+FROM products
+GROUP BY productline;
+
+-- Tổng số tiền thu được từ mỗi dòng sản phẩm
+SELECT o.productCode, productName, sum(priceEach * quantityOrdered) total
+FROM orderdetails as o
+         join products p on p.productCode = o.productCode
+GROUP by productCode
+ORDER BY total DESC;
+
+-- Những đơn hàng trên $40,000
+SELECT ordernumber,
+       sum(quantityOrdered)             AS itemsCount,
+       sum(priceEach * quantityOrdered) AS total
+FROM orderdetails
+GROUP BY ordernumber
+HAVING total > 40000;
+
+-- Lượng khách hàng tại từng thành phố
+SELECT city, count(customerNumber) as customerCount
+FROM customers
+GROUP BY city
+HAVING customerCount > 1;
+
+-- Tổng giá trị các đơn hàng của các khách hàng
+SELECT C.customerName,
+       sum(OD.priceEach * OD.quantityOrdered)
+           as total
+FROM customers C
+         JOIN orders O on C.customerNumber = O.customerNumber
+         JOIN orderdetails OD on O.orderNumber =
+                                 OD.orderNumber
+GROUP BY C.customerName
+HAVING total > 100000
+ORDER BY total DESC;
+
+-- Các khách hàng không đặt đơn hàng nào
+SELECT c.customerNumber, customerName
+FROM customers c
+         LEFT JOIN orders o ON c.customerNumber = o.customerNumber
+WHERE orderNumber is NULL;
+
+-- (Self Join) khi một bảng có khóa ngoài tham chiếu tới khóa chính của chính nó
+-- Ví dụ: Bảng employees có một khóa ngoài là reportsTo tham chiếu tới khóa chính employeeNumber của chính bảng employees
+SELECT concat(e1.lastName, ' ', e1.firstName) as fullname,
+       e1.email,
+       concat(e2.lastName, ' ', e2.firstName) as manager,
+       e2.email                               as manager_email
+FROM employees e1 JOIN employees e2 ON e1.reportsTo = e2.employeeNumber;
+-- hoặc có thể viết
+SELECT concat(e1.lastName, ' ', e1.firstName) as fullname,
+       e1.email,
+       concat(e2.lastName, ' ', e2.firstName) as manager,
+       e2.email                               as manager_email
+FROM employees e1,
+     employees e2
+WHERE e1.reportsTo = e2.employeeNumber;
+
+-- thông tin về các nhân viên và tên văn phòng nơi họ làm việc
+select concat(e.lastName, ' ', e.firstName) as fullname, o.city as office
+from employees e
+         join offices o on o.officeCode = e.officeCode;
+
+-- tên khách hàng và tên các sản phẩm họ đã mua
+select c.customerName, p.productName
+from customers c
+         join orders o on c.customerNumber = o.customerNumber
+         join orderdetails od on o.orderNumber = od.orderNumber
+         join products p on od.productCode = p.productCode;
+
+-- các mặt hàng chưa có ai đặt mua
+select p.productName, od.orderNumber
+from products p
+         left join orderdetails od on p.productCode = od.productCode
+where od.orderNumber is null;
+
+-- các đơn hàng trong tháng 3/2005 (gồm orderDate, requiredDate, Status) và tổng giá trị của mỗi đơn hàng
+select o.orderNumber, o.orderDate, o.requiredDate, o.status, sum(od.priceEach * od.quantityOrdered) as total
+from orders o
+         join orderdetails od on o.orderNumber = od.orderNumber
+where o.orderDate between '2005-03-01' and '2005-03-31'
+group by o.orderNumber;
+
+-- thông tin về các dòng sản phẩm và số lượng sản phẩm của dòng sản phẩm đó
+select productName,
+       quantityInStock,
+       COALESCE(quantityOrdered, 0)                     AS quantityOrdered,
+       (quantityInStock + COALESCE(quantityOrdered, 0)) as total
+from products p
+         left join orderdetails od on p.productCode = od.productCode
+order by total desc;
+
+-- Đưa ra các đơn hàng gần đây nhất
+SELECT *
+FROM orders
+WHERE orderDate = (SELECT MAX(orderDate) FROM orders);
+
+-- các sản phẩm không có mặt trong bất kỳ đơn hàng nào
+SELECT *
+FROM products
+WHERE productCode NOT IN (SELECT productCode FROM orderdetails);
+
+
+-- các sản phẩm có số lượng trong kho lớn hơn trung bình
+SELECT *
+FROM products p
+WHERE quantityInStock >
+      (SELECt avg(quantityInStock)
+       FROM products);
+
+-- với mỗi dòng đơn hàng, đưa vào thêm tên của sản phẩm
+SELECT orderNumber,
+       quantityOrdered,
+       (SELECT productName
+        FROM products
+        WHERE productCode =
+              o.productCode) as productName
+FROM orderdetails o;
+--  tương đương với
+SELECT orderNumber, quantityOrdered, productName
+FROM orderdetails o
+         JOIN products p on o.productCode = p.productCode;
+
+-- với mỗi sản phẩm, đưa kèm thêm tổng số lượng sản phẩm đó đã được đặt hàng
+SELECT productName,
+       (SELECT sum(quantityOrdered)
+        FROM orderdetails
+        WHERE productCode = p.productCode) as totalQuantityOrderd
+FROM products as p
+ORDER BY totalQuantityOrderd desc;
+
+-- Các sản phẩm có đơn đặt hàng trong tháng 3/2005
+SELECT productName
+FROM products
+WHERE productCode IN (SELECT productCode
+                      FROM orderdetails
+                      WHERE orderNumber IN (SELECT orderNumber
+                                            FROM orders
+                                            WHERE orderDate BETWEEN '2005-03-01' AND '2005-03-31'));
+-- dùng join
+SELECT p.productName
+FROM products AS p
+         JOIN orderdetails AS od using (productCode)
+         JOIN orders AS o using (orderNumber)
+WHERE o.orderDate BETWEEN '2005-03-01' AND '2005-03-31';
+
+-- information about orders in the last month
+SELECT *
+FROM orders
+WHERE orderDate >= (select DATE_ADD(MAX(orderDate), INTERVAL -1 MONTH) from orders);
+
+-- Use a subquery that gives information about orders and total order value
+select *,
+       (select sum(priceEach * quantityOrdered)
+        from orderdetails
+        where orderNumber = o.orderNumber) as totalOrderValue
+from orders o;
+
+-- For each customer, give the total amount of the goods, and the total amount they paid
+SELECT c.customerName,
+       SUM(od.priceEach * od.quantityOrdered) AS totalOrderValue,
+       SUM(p.amount)                          AS totalPayment
+FROM customers c
+         JOIN orders o USING (customerNumber)
+         JOIN orderdetails od USING (orderNumber)
+         JOIN payments p USING (customerNumber)
+GROUP BY c.customerName;
+
+-- cập nhật email của Diane Murphy với employeeNumber là 1002 thành diane-murphy@classicmodelcars.com
+UPDATE employees
+SET email = 'diane-murphy@classicmodelcars.com'
+WHERE employeeNumber = 1002;
+
+SELECT firstname,
+       lastname,
+       email
+FROM employees
+WHERE employeeNumber = 1002;
+
+-- Sửa các nhân viên có titleJob là ‘Sales Rep’ thành ‘Sales Representative’
+UPDATE employees
+SET jobTitle = 'Sales Representative'
+WHERE jobTitle = 'Sales Rep';
+
+-- Create a table named temp_orderDetails, then do more data in the most recent date from the orderdetails table to the temp table
+DROP TABLE temp_orderdetails;
+
+CREATE TABLE IF NOT EXISTS temp_orderDetails AS
+SELECT od.*
+FROM orderdetails od
+         JOIN (
+    SELECT orderNumber
+    FROM orders
+    WHERE orderDate = (SELECT MAX(orderDate) FROM orders)
+) o ON od.orderNumber = o.orderNumber;
+
+SELECT * FROM temp_orderDetails;
 
